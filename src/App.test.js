11bc40c951eb78a685032fa41d292291b39d0c04
sagarlimbu0 <sagarlimbu027@gmail.com>
2023-01-03 11:37:30 -0800

    changes on tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,3 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-
-test('renders learn react link',()=>{
-	render(<App />);
-	const linkElement= screen.getByText(/lean react/i);
-	expect(linkElement).toBeInTheDocument();
-});
